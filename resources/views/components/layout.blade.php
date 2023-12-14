becac436311a5249325153e10fb54581232040d8
Oryuta13 <ryuta1113abcdol@gmail.com>
2023-12-15 02:34:59 +0900

    pictureの投稿機能の実装

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,10 +6,9 @@
         content="width=device-width, user-scalable=no, initial-scale=1.0,
         maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link href="/css/app.css" rel="stylesheet">
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
   <title>{{ $title ?? 'つぶやきアプリ' }}</title>
   @stack('css')
-  @vite('resources/css/app.css')
 </head>
 <body class="bg-gray-200">
   {{ $slot }}
